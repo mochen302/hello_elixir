f74be511567787ba4c6eacbc12c6297235e77997
mochen302 <mochen302@163.com>
2018-06-25 17:14:40 +0800

    add more test

--- lib/example.ex
+++ lib/example.ex
@@ -4,4 +4,20 @@ defmodule Example do
     IO.puts("Time: #{time} μs")
     IO.puts("Result: #{result}")
   end
+
+  def add(a, b) do
+    IO.puts(a + b)
+  end
+
+  def listen do
+    receive do
+      {:ok, "hello"} -> IO.puts("world")
+    end
+    listen()
+  end
+
+  def double(x) do
+    :timer.sleep(2000)
+    x * 2
+  end
 end
---
+++ lib/simple_queue.ex
@@ -0,0 +1,20 @@
+defmodule SimpleQueue do
+  use GenServer
+
+  def init(state), do: {:ok, state}
+
+  def handle_call(:dequeue, _from, [value | state]), do: {:reply, value, state}
+
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+
+end
